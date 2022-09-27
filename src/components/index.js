b0cefae2dc831841795269171821649e1b40feb2
Maria210194 <mary.stachi@gmail.com>
2022-09-28 00:32:58 +0200

    Init + new dependencies + components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Feed';
+export { default as ProductDetail } from './ProductDetail';
+export { default as SearchFeed } from './SearchFeed';
